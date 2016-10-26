dee57e79af23f03eea0b3af940a3ab253c3ba72d
disinvite <disinvite@users.noreply.github.com>
2016-10-25 22:22:21 -0400

    crappy support for multi-line

--- wowgif.py
+++ wowgif.py
@@ -1,12 +1,25 @@
 import math
 from PIL import Image, ImageFont, ImageDraw
 
-def get_stamp(text):
+def get_stamp(lines):
     font = ImageFont.truetype("times.ttf", 1000)
-    x,y = font.getsize(text)
+    
+    sizes = [font.getsize(text) for text in lines]
+    x = max(t[0] for t in sizes)
+    y = sum(t[1] for t in sizes)
+    
+    ty = 0
+    y_offset = []
+    for t in sizes[::-1]:
+        y_offset += [ty]
+        ty += t[1]
+    
     image = Image.new('RGB',(x,y))
-    draw = ImageDraw.Draw(image)
-    draw.text((0, 0), text, font=font, fill=(244,34,114,0))
+    for i,text in enumerate(lines):
+        draw = ImageDraw.Draw(image)
+        pos = ( int(0.5 * (x - sizes[i][0])), y_offset[i] )
+        draw.text(pos, text, font=font, fill=(244,34,114,0))
+    
     return image.crop(image.getbbox())
 
 def rotated_frame(text,horiz_size,angle):
@@ -28,12 +41,11 @@ def rotated_frame(text,horiz_size,angle):
     return image
     
 if __name__ == '__main__':
-    rotate_pts = [20 * math.cos(i * math.pi / 180.0) for i in range(0,360)]
+    rotate_pts = [10 * math.cos(i * math.pi / 180.0) for i in range(0,360)]
     zoom_pts   = [100 * math.sin(i * math.pi / 180.0) for i in range(0,360)]
     ns = range(0,360,10)
-    print ns
     
     for i, v in enumerate(ns):
-        x = rotated_frame('TEST',500 + int(zoom_pts[v]), rotate_pts[v])
+        x = rotated_frame(['hi','there'],500 + int(zoom_pts[v]), rotate_pts[v])
         filename = 'test/a/bla{}.png'.format(i)
         x.save(filename, "PNG")
