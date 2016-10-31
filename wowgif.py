f114d785afe8e685f512838128dc6d3ccf8f25bb
disinvite <disinvite@users.noreply.github.com>
2016-10-30 21:18:40 -0400

    multi-element test

--- wowgif.py
+++ wowgif.py
@@ -61,14 +61,20 @@ def create(canvas,frames):
         
     ffmpeg('bla','test.gif')
     
-if __name__ == '__main__':
-    canvas = {'x': 400, 'y': 400}
+def dummy_up_one():
     rotate_pts = [10 * math.cos(i * math.pi / 180.0) for i in range(0,360)]
-    #zoom_pts   = [int( 300 + (50 * math.sin(i * math.pi / 180.0)) ) for i in range(0,360)]
     zoom_pts = [300 for i in range(360)]
     y_pts = [200 + 30 * math.cos(i * math.pi / 180.0) for i in range(0,360)]
+    return [{'text': 'HEY NOW', 'zoom': zoom_pts[i], 'angle': rotate_pts[i], 'x':200, 'y': y_pts[i]} for i in range(0,360,10)]
     
-    frames = [ {'objs': [{'text': 'HEY NOW', 'zoom': zoom_pts[i], 'angle': rotate_pts[i], 'x':200, 'y': y_pts[i]}] } for i in range(0,360,10)]
+def dummy_up_two():
+    x_pts = [260 + 30 * math.cos(i * math.pi / 180.0) for i in range(0,360)]
+    return [{'text': 'lil guy', 'zoom': 100, 'angle': 0, 'x':x_pts[i], 'y': 350} for i in range(0,360,10)]
+    
+if __name__ == '__main__':
+    canvas = {'x': 400, 'y': 400}
+    xx = zip(dummy_up_one(),dummy_up_two())
+    frames = [ {'objs': x} for x in xx ]
     create(canvas,frames)
     ffmpeg('bla','test.gif')
     
