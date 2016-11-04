c7725ac2721d14891785f8f284699f8c3723f0bc
disinvite <disinvite@users.noreply.github.com>
2016-11-03 22:17:52 -0400

    dump ffmpeg for imagemagick

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ def dummy_up_one():
     rotate_pts = [10 * math.cos(i * math.pi / 180.0) for i in range(0,360)]
     zoom_pts = [300 for i in range(360)]
     y_pts = [200 + 30 * math.cos(i * math.pi / 180.0) for i in range(0,360)]
-    return [{'text': 'HEY NOW', 'zoom': zoom_pts[i], 'angle': rotate_pts[i], 'x':200, 'y': y_pts[i]} for i in range(0,360,10)]
+    return [{'text': 'HEY NOW', 'color': '#f80000', 'zoom': zoom_pts[i], 'angle': rotate_pts[i], 'x':200, 'y': y_pts[i]} for i in range(0,360,10)]
     
 def dummy_up_two():
     x_pts = [260 + 30 * math.cos(i * math.pi / 180.0) for i in range(0,360)]
-    return [{'text': 'lil guy', 'zoom': 100, 'angle': 0, 'x':x_pts[i], 'y': 350} for i in range(0,360,10)]
+    return [{'text': 'lil guy', 'color': '#c0c0ff', 'zoom': 100, 'angle': 0, 'x':x_pts[i], 'y': 350} for i in range(0,360,10)]
 
 if __name__ == '__main__':    
     canvas = {'filename': 'hi', 'x': 400, 'y': 400}
--- wowgif.py
+++ wowgif.py
@@ -3,7 +3,7 @@ import random
 from subprocess import Popen
 from PIL import Image, ImageFont, ImageDraw
 
-def id_generator(size=8, chars=string.ascii_lowercase + string.digits):
+def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def color_hex_to_rgba(x):
@@ -61,19 +61,19 @@ def one_frame(canvas,frame):
     for obj in frame['objs']:
         image = render(image,obj)
     return image
-
-def ffmpeg(frame_prefix,filename):
-    x = 'H:/work2/wowgif/temp/{}%d.png'.format(frame_prefix)
-    process = Popen(['D:/bin/ffmpeg-20151126-git-72eaf72-win64-static/bin/ffmpeg.exe',
-    '-loglevel','panic',
-    '-y','-r','30','-i',x,filename])
+    
+def convert(frame_prefix,filename):
+    im = 'bin/convert.exe'
+    x = 'temp/{}*.png'.format(frame_prefix)
+    process = Popen([im,'-delay','3','-loop','0',x,filename])
+    process.wait()
     
 def create(canvas,frames):
     temp_id = id_generator()
     for i, frame in enumerate(frames):
         x = one_frame(canvas,frame)
-        filename = 'temp/{}{}.png'.format(temp_id,i)
+        filename = 'temp/{0}_{1:04d}.png'.format(temp_id,i)
         x.save(filename, "PNG")
         
     outfile = '{}.gif'.format(canvas['filename'])
-    ffmpeg(temp_id,outfile)
+    convert(temp_id,outfile)
